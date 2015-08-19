c79343675575d464ee00d9e770582e3dec947556
jrthorne <jthorne@magiclamp.com.au>
2015-08-19 10:11:57 +1000

    Put all the applications in project

--- project/simon/models.py
+++ project/simon/models.py
@@ -1,3 +1,17 @@
+#################################################################
+FileName        = 'models.py'
+# By:            Jason Thorne
+# Date:            19-08-2015
+# Description:     The simon project
+##################################################################
+import datetime, pytz
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import Max, Min, Q 
+from django.http import Http404
 
 # Create your models here.
+class Player(models.Model):
+	# one to one with django user. You can select any user to be a player 
+	user = models.OneToOneField(User)
+	games_played
